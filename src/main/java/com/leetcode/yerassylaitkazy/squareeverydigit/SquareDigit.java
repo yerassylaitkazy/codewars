2a66303ea72672c3f35804cbe63496b9dfb35887
eitan.aizek <eitan.aizek@techork.com>
2022-11-04 23:19:21 +0500

    lc: implemented Square Every Digit

--- src/main/java/com/leetcode/yerassylaitkazy/squareeverydigit/SquareDigit.java
+++ src/main/java/com/leetcode/yerassylaitkazy/squareeverydigit/SquareDigit.java
@@ -1,8 +1,29 @@
 package com.leetcode.yerassylaitkazy.squareeverydigit;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SquareDigit {
 
-  public int squareDigits(int n) {
-    return 0;
+  private static final String DELIMITER = "";
+
+  public int squareDigits(int number) {
+    return Integer.parseInt(getDoubledDigitsString(number));
+  }
+
+  private String getDoubledDigitsString(int number) {
+    return getStreamOfDigits(number)
+      .map(this::squareNumber)
+      .map(String::valueOf)
+      .collect(Collectors.joining(DELIMITER));
+  }
+
+  private Stream<Integer> getStreamOfDigits(int number) {
+    return Arrays.stream(String.valueOf(number).split(DELIMITER)).map(Integer::parseInt);
+  }
+
+  private int squareNumber(int number) {
+    return number * number;
   }
 }
