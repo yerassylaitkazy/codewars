03aed33253a64d2713be078acea49105a065388f
eitan.aizek <eitan.aizek@techork.com>
2022-11-17 21:23:27 +0500

    cw: resolved HumanReadableTime

--- src/main/java/com/leetcode/yerassylaitkazy/humanreadabletime/HumanReadableTime.java
+++ src/main/java/com/leetcode/yerassylaitkazy/humanreadabletime/HumanReadableTime.java
@@ -3,6 +3,10 @@ package com.leetcode.yerassylaitkazy.humanreadabletime;
 public class HumanReadableTime {
 
   public static String makeReadable(int seconds) {
-    return null;
+    int hh = seconds / 3600;
+    int mm = (seconds - hh * 3600) / 60;
+    int ss = (seconds - hh * 3600) - mm * 60;
+
+    return String.format("%02d:%02d:%02d", hh, mm, ss);
   }
 }
